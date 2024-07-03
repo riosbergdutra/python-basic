54b742aadcd1fef46f98acd1212f7da9afe197c1
Riosberg Dutra <riosbergduttra@gmail.com>
2024-07-03 10:09:54 -0300

    estrutura condicional dentro de outra

---
+++ arquivosPython/estrutura_condicional_aninhada.py
@@ -0,0 +1,18 @@
+conta_normal = True
+conta_universitaria = False
+
+saldo = 2000
+saque = 500
+cheque_especial = 450
+
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com sucesso!")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("saldo insuficiente!")        
