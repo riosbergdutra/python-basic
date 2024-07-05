4719b9394fbafc03da17c6b276c1cc6faac53662
Riosberg Dutra <riosbergduttra@gmail.com>
2024-07-05 14:07:24 -0300

    fitamento de string

---
+++ arquivosPython/manipularStr/string_3.py
@@ -0,0 +1,31 @@
+nome = "Guilherme Arthur de Carvalho"
+
+
+print(nome[0])
+print(nome[-1])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[:: 1]) 
+
+# nome[0]
+# >>> "6"
+
+# nome[:9]
+# >>> "Guilherme"
+
+# nome[10:]
+# >>> "Arthur de Carvalho"
+
+# nome[10:16]
+# >>> "Arthur"
+
+# nome:[10:16:2]
+# >>> "Atu"
+
+# nome[:]
+# >>> "Guilherme Arthur de Carvalho"
+
+# nome[:: 1] 
