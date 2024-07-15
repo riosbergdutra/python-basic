5baeff6f3f4c3526792ead902c742d6463d699a4
Riosberg Dutra <riosbergduttra@gmail.com>
2024-07-15 15:18:39 -0300

    datetime e date

---
+++ orientacao_objetos/data-hora-e-fusohorario/meu_script.py
@@ -0,0 +1,7 @@
+from datetime import date, datetime
+data = date(2023, 7, 10)  # definindo somente a data
+print(data)
+print(data.today())  # mostra a data de agora
+
+data_hora = datetime(2023, 7, 10, 10, 30, 20)  # definindo data e hora
+print(data_hora)
